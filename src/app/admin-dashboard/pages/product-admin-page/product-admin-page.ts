fd80ff46a849d90abdf28364456855c58a468aba
Jrdb03 <jregaladcv@alumnos.unex.es>
2026-05-28 09:16:45 +0200

    feat: :sparkles: add new funcionalities

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from '@products/services/products.service';
+import { ProductDetails } from "./product-details/product-details";
 
 @Component({
   selector: 'app-product-admin-page',
-  imports: [],
+  imports: [ProductDetails],
   templateUrl: './product-admin-page.html',
 })
-export class ProductAdminPage {}
+export class ProductAdminPage {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  productService = inject(ProductsService);
+
+  productId = toSignal(
+    this.activatedRoute.params.pipe(
+      map((params) => params['id']))
+  );
+
+  productResource = rxResource({
+    request: () => ({ id: this.productId() }),
+    loader: ({ request }) => {
+      return this.productService.getProductById(request.id);
+    }
+  });
+
+  redirectEffect = effect(() => {
+    if(this.productResource.error()){
+      this.router.navigate(['/admin/products']);
+    }
+  })
+
+}
